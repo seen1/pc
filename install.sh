67b91aec8e67b30197e36af2f8c3733baea65746
seen1 <93178753+seen1@users.noreply.github.com>
2024-04-28 11:07:49 +0800

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "1
 10
-31535
+55555
 
 
 
-" | bash <(curl -s -L https://raw.githubusercontent.com/xyz690/v2ray/master/install.sh)
+" | bash <(curl -s -L https://raw.githubusercontent.com/seen1/pc/main/install_ori.sh)
 v2ray url
